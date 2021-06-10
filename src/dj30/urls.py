ab3b358ea51cf327aaa432700f5d40383a64b1d2
YogPanjarale <yog.panjarale@gmail.com>
2021-06-10 11:30:58 +0530

    basic post django tempelets

--- src/dj30/urls.py
+++ src/dj30/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from post.views import post_list_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/',post_list_view)
 ]
--- src/post/views.py
+++ src/post/views.py
@@ -1,3 +1,14 @@
+from django.http.request import HttpRequest
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
+
+#CRUD -> Create Retrive Update Delete
+
+def post_list_view(request:HttpRequest):
+    post_ojects = Post.objects.all() 
+    context = {
+        'post_objects':post_ojects
+    }
+    return render(request,"posts/index.html",context)
